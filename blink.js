9845c1086d08f03d079fab42ca969ed64977821e
sFlcn <sfalcn@gmail.com>
2022-11-06 22:17:45 +0300

    Smooth blinks

--- blink.js
+++ blink.js
@@ -1,18 +1,30 @@
-export function starsBlink() {
-  this.stars.push({
+function makeNewStar() {
+  const starPower = Math.floor(Math.random() * 150) + 50;
+  return {
     x: Math.floor(Math.random()*1600),
     y: Math.floor(Math.random()*800),
-    size: Math.floor(Math.random()*1) + 1,
-    pow: Math.floor(Math.random()*300)
-  })
+    size: 1 + Math.floor(Math.random()*2),
+    initPower: starPower,
+    power: starPower,
+  };
+}
+
+function drawStar(star) {
+  const starPhase = ((star.initPower - Math.abs(star.initPower - star.power * 2)) / star.initPower) ** 1.5;
+  this.ctx.beginPath();
+  this.ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2, true);
+  this.ctx.closePath;
+  this.ctx.fillStyle = `rgba(135, 132, 227, ${starPhase})`;
+  this.ctx.fill();
+}
+
+export function starsBlink() {
+  this.stars.push(makeNewStar());
+  this.stars.push(makeNewStar());
   for (let i = this.stars.length - 1; i >= 0; i--) {
-    const star = this.stars[i];
-    if (star.pow-- > 0) {
-      this.ctx.beginPath();
-      this.ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2, true);
-      this.ctx.closePath;
-      this.ctx.fillStyle = 'rgba(115,112,227,0.9)';
-      this.ctx.fill();
+    if (this.stars[i].power) {
+      drawStar.bind(this)(this.stars[i]);
+      this.stars[i].power -= 1;
     } else {
       this.stars.splice(i, 1);
     }
